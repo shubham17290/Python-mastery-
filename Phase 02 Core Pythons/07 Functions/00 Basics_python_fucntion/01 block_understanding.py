fc0398aae772f7f53c6fae9c8836b83b8fee698d
Gian <smourya1046@gmail.com>
2026-07-06 13:40:22 +0530

    functions logic building

---
+++ Phase 02 Core Pythons/07 Functions/00 Basics_python_fucntion/01 block_understanding.py
@@ -0,0 +1,15 @@
+def welcome():
+    print("hi guy's")
+    print("i am welcoming you all")
+    print("in my course of the python mastery")
+welcome() # this particular stuff will going to call the whole welcome function and will return the stuffs that are embedded inside of it !
+
+"""
+1 . Function did not execute's automatically it executes only when it is called 
+2.  is every function returns a value ?
+ans : No , some only perform an action !
+
+3. Functions are only for large programs ?
+ans  : No --> Even a small to small programs can carry - function .
+
+"""
---
+++ Phase 02 Core Pythons/07 Functions/01 Basics_python_input_an_outputd.py
@@ -0,0 +1,2 @@
+age = int(input("Enter your age: "))
+print(f"You will turn 100 in {100 - age} years!")
